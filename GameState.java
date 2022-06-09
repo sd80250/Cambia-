3c3361e6e86bd290e5db18ccc963c2960ce9d8d8
Shawn Duan <sd80250@gmail.com>
2022-06-08 19:49:33 -0700

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -14,7 +14,7 @@ class GameState {
 	public Player whoCalledCambia() {
 		return currentPlayer;
 	}
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		List<Card> deck1 = new ArrayList<>();
 		for (int i = 0; i < 52; i+=4) {
 			deck1.add(new Card(i/4, 0));
@@ -24,8 +24,39 @@ class GameState {
 		}
 		Deck deck = new Deck(deck1);
 	}
+
+	// terminal methods
+	private int decideOnMove() {}
+	private final int CALL_CAMBIA = 0;
+	private final int DRAW_FROM_DECK = 1;
+	private final int DRAW_FROM_DISCARD = 2;
+
+	// game state methods
 	private void callCambia() {}
 	private void drawFromDeck() {}
 	private void drawFromDiscard() {}
-	public void turn() {}
+
+	// end of game methods
+	private void countScore() {}
+	private void declareWinner() {}
+	private void endGame() {}
+
+	public void turn() {
+		if (isCambiaCalled()) {
+			if whoCalledCambia().equals(currentPlayer()) {
+				countScore();
+				declareWinner();
+				endGame();
+			} else {
+				switch (decideOnMove()) {
+				case CALL_CAMBIA:
+					callCambia();
+				case DRAW_FROM_DECK:
+					drawFromDeck();
+				case DRAW_FROM_DISCARD:
+					drawFromDiscard();
+				}
+			}
+		}
+	}
 }
