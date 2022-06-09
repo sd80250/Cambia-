a858ee28e0c9d1a53eb4c2a682a020bd31202ec5
Shawn Duan <sd80250@gmail.com>
2022-06-08 18:09:50 -0700

    added variables to Deck and GameState

--- Deck.java
+++ Deck.java
@@ -1,9 +1,18 @@
 import java.util.List;
 
 class Deck {
-	List<Card> deck;
-
+	private List<Card> deck;
+	public Deck(List<Card> deck) {
+		this.deck = deck;
+	}
 }
+
 class Card {
-	
+	private int number;
+	private int suite; 
+
+	public Card(int number, int suite) {
+		this.number = number;
+		this.suite = suite;
+	}
 }
--- GameState.java
+++ GameState.java
@@ -1,3 +1,15 @@
-class GameState{
+class GameState {
+	private Deck[] playerHands;
+	private Deck drawPile;
+	private Card topDiscardCard;
+	private Player cambiaCaller;
+	private Player currentPlayer;
+	public boolean isCambiaCalled() {
+		return currentPlayer != null;
+	}
+	public Player whoCalledCambia() {
+		if isCambiaCalled() return currentPlayer;
+		return null;
+	}
 	
 }
